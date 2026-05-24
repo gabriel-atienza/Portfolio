f77f2b86e689cef4191ba38dd8b36165bff08db8
gabriel-atienza <gabrielatienza.dev@gmail.com>
2026-05-25 06:50:34 +0800

    Update App.jsx
    added certs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Hero       from "./components/Hero";
-import CareerPath from "./components/CareerPath";
+import Hero           from "./components/Hero";
+import CareerPath     from "./components/CareerPath";
+import Certifications from "./components/Certifications";
 
 export default function App() {
   return (
     <main>
       <Hero avatarSrc="/gabriel.jpg" />
       <CareerPath />
+      <Certifications />
     </main>
   );
 }
