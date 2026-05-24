2ea777ea41d77c37c0d25ee765333ef43b37c4a7
gabriel-atienza <gabrielatienza.dev@gmail.com>
2026-05-25 06:46:07 +0800

    Update App.jsx
    added career path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Hero from "./components/Hero";
+import Hero       from "./components/Hero";
+import CareerPath from "./components/CareerPath";
 
 export default function App() {
   return (
     <main>
       <Hero avatarSrc="/gabriel.jpg" />
+      <CareerPath />
     </main>
   );
 }
